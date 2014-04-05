9083b32a82ca0c7945066d15b3d0ad78b172e13d
unknown-user <unknown-user@fe80::4d0:c8fd:ce37:8926%15>
2014-04-05 23:24:25 +0530

    Initial check-in of module AutoFareCheck

---
+++ AutoFareCalculator/AutoFareCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutoFareCalculator
+{
+	public class AutoFareCalculator
+	{
+		public AutoFareCalculator ()
+		{
+		}
+
+		public string CalculateFare(double distance)
+		{
+			double fare = (distance < 1.9) ? 25 : (distance - 1.9) * 13 + 25;
+			return string.Format("{0:0.00}", fare);
+		}
+	}
+}
+
---
+++ AutoFareCheck/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using AutoFareCalculator;
+
+namespace AutoFareCheck
+{
+	[Activity (Label = "AutoFareCheck", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			AutoFareCalculator.AutoFareCalculator fareCalculator = new AutoFareCalculator.AutoFareCalculator ();
+
+			// Set our view from the "main" layout resource
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton2);
+			EditText editText = FindViewById<EditText> (Resource.Id.textView2);
+			TextView resultText = FindViewById<TextView> (Resource.Id.textView3);
+
+
+
+			button.Click += delegate {
+				var resultFare = fareCalculator.CalculateFare(double.Parse(editText.Text));
+				resultText.Text = string.Format ("Your fare is Rs.{0} ", resultFare);
+			};
+		}
+	}
+}
+
+
